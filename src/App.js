07e5622b3719f403be699a47f228bdab79cc0e2b
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-22 09:26:11 +0300

    added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className=' drop-shadow-primary'>
+      <h1 className=' font-bold'>
+        <a href='#'>Link 1</a>
+        <a href='#'>Link 2</a>
+        <a href='#'>Link 3</a>
+      </h1>
+      <button className='btn'>Learn more</button>
+    </div>
+  );
+};
+
+export default App;
